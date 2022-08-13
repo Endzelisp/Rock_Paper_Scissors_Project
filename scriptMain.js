a0bf8dfae52fff3cb4ef005a6b11642578e53ae1
Armando Endzelis <endzelisp@gmail.com>
2022-08-13 00:16:39 -0400

    Create playerSelection function

--- scriptMain.js
+++ scriptMain.js
@@ -3,12 +3,24 @@ function getComputerChoice() {
 
   switch (randomNum) {
     case 1:
-      return 'paper'
-    case 2:
-      return 'scissors'
-    case 3:
       return 'rock'
-  }
+    case 2:
+      return 'paper'
+    case 3:
+      return 'scissors'
+  };
 }
 
+function playerSelection() {
+  let playerSelection = prompt('Type your selection: rock, paper, scissors');
+  playerSelection = playerSelection.toLowerCase()
+  if (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
+    console.log('Wrong Imput');
+    return
+  };
+  return playerSelection
+};
+
 console.log(getComputerChoice())
+
+console.log(playerSelection())
