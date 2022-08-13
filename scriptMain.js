c5a1da7cc665b717e886b4dab4355b5cdd4294ca
Armando Endzelis <endzelisp@gmail.com>
2022-08-12 23:25:34 -0400

    Create getComputerChoice function

---
+++ scriptMain.js
@@ -0,0 +1,14 @@
+const getComputerChoice = () => {
+  let randomNum = Math.floor(Math.random() * 3) + 1;
+
+  switch (randomNum) {
+    case 1:
+      return 'paper'
+    case 2:
+      return 'scissors'
+    case 3:
+      return 'rock'
+  }
+}
+
+console.log(getComputerChoice())
