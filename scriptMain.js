be24fc4e21268a293fa19d8483a22998a4955562
Armando Endzelis <endzelisp@gmail.com>
2022-08-30 21:35:54 -0400

    Refactor playRound function
    Simplify and improve function logic

--- scriptMain.js
+++ scriptMain.js
@@ -11,42 +11,41 @@ function getComputerChoice() {
   };
 }
 
+let gameRules = [['paper', 'rock'], ['scissors', 'paper'], ['rock', 'scissors']]
+
+
 function playRound(playerSelection) {
-  let computerChoice = getComputerChoice()
-  if (computerChoice === 'rock' && playerSelection === 'paper') {
-    return 'You Won! Paper beats Rock'
-  } else if (computerChoice === 'paper' && playerSelection === 'rock') {
-    return 'You Lose! Paper beats Rock'
+  let playerWin = false;
+  let computerChoice = getComputerChoice();
+  console.log(`player: ${playerSelection} | computer: ${computerChoice}`);
+  
+  if (playerSelection === computerChoice) return `It's a tie`;
+
+  for (let i = 0; i < 3; ++i) {
+    if (playerSelection === gameRules[i][0] && computerChoice === gameRules[i][1]) {
+      playerWin = true
+      break
+    }
   }
 
-  else if (computerChoice === 'scissors' && playerSelection === 'rock') {
-    return 'You Won! Rock beats Scissors'
-  } else if (computerChoice === 'rock' && playerSelection === 'scissors') {
-    return 'You Lose! Rock beats Scissors'
+  if (playerWin) {
+    return `You won this round ${playerSelection} beats ${computerChoice}`
   }
-
-  else if (computerChoice === 'paper' && playerSelection === 'scissors') {
-    return 'You Won! Scissors beats Paper'
-  } else if (computerChoice === 'scissors' && playerSelection === 'paper') {
-    return 'You Lose! Scissors beats Paper'
-  }
-  else if (computerChoice === playerSelection) {
-    return 'It\'s a tie'
-  }
+  return `You lose this round ${computerChoice} beats ${playerSelection}` 
 }
 
 
 const results = document.querySelector('.results')
 
-let availableTurns = 5;
+//let availableTurns = 5;
 
 
 addEventListener('click', (e) => {
-  if (availableTurns > 0) {
+//  if (availableTurns > 0) {
     let elemTarget = e.target;
     let playerChoice = elemTarget.id;
-    let round = playRound(playerChoice)
-    results.textContent = `${round} ${availableTurns} turns left`
-    --availableTurns
-  }
+    let roundResult = playRound(playerChoice)
+    results.textContent = `${roundResult}`
+   // --availableTurns
+ // }
 })
